25531bee95ea6823a3ea0a1aa3968fc1148959db
Jayanta Debnath <jayanta.debnath@in.bosch.com>
2025-06-30 12:27:10 +0530

    some more restore

--- restore.sh
+++ restore.sh
@@ -3,6 +3,12 @@
 echo "alias python='python3.10'" >> ~/.bashrc
 echo "export PATH=\$PATH:/home/jayanta/Tools/flutter" >> ~/.bashrc
 
+# set timezone
+sudo timedatectl set-timezone Asia/Kolkata
+
+# install python 3.10
+# TODO
+
 # install joplin
 sudo apt update && sudo apt upgrade
 sudo apt install libfuse2
@@ -13,3 +19,8 @@ sudo apt install rclone
 rclone config
 ./mount_cloud_drives.sh
 
+# modify the prompt
+echo 'PROMPT_COMMAND='\''PS1="[\$(date +%H:%M)] \${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "'\''' >> ~/.bashrc
+
+# other changes to .bashrc
+echo "alias python='python3.10'" >> ~/.bashrc
