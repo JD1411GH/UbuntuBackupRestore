4d1edd7adb5c4d12d9220bae270b9b8edff0936d
Jayanta <jayanta.dn@gmail.com>
2025-07-15 13:59:10 +0530

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "alias python='python3.10'" >> ~/.bashrc
+# backup .bashrc
+cp ~/.bashrc ~/.bashrc.orig
+
+# TODO flutter installation
 echo "export PATH=\$PATH:/home/jayanta/Tools/flutter" >> ~/.bashrc
 
 # set timezone
@@ -8,16 +11,17 @@ sudo timedatectl set-timezone Asia/Kolkata
 
 # install python 3.10
 # TODO
+echo "alias python='python3.10'" >> ~/.bashrc
 
 # install joplin
 sudo apt update && sudo apt upgrade
 sudo apt install libfuse2
 wget -O - https://raw.githubusercontent.com/laurent22/joplin/dev/Joplin_install_and_update.sh | bash
 
-# install apt packages
+# install necessary apt packages
 sudo apt install default-jdk google-android-platform-tools-installer
 
-# install deb packages
+# TODO install deb packages
 
 # map cloud drives
 sudo apt install rclone
@@ -26,6 +30,3 @@ rclone config
 
 # modify the prompt
 echo 'PROMPT_COMMAND='\''PS1="[\$(date +%H:%M)] \${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]\$ "'\''' >> ~/.bashrc
-
-# other changes to .bashrc
-echo "alias python='python3.10'" >> ~/.bashrc
